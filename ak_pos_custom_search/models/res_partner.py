72dea04d246b06474cad243ac8628dbf8cac7839
aktiv-ashray-joshi <ashray.joshi@aktivsoftware.com>
2026-04-13 18:17:33 +0530

    [UPD]| Added a functionality for unique Code and code optimizibility

--- ak_pos_custom_search/models/res_partner.py
+++ ak_pos_custom_search/models/res_partner.py
@@ -4,10 +4,16 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
     
 
-    ak_customer_code = fields.Char(string="Customer Code",placeholder="Enter Customer Code")
+    ak_customer_code = fields.Char(string="Customer Code")
+
+    _unique_ak_customer_code = models.Constraint(
+        'unique(ak_customer_code)',
+        "Customer Code must be unique",
+    )
 
     @api.model
     def _load_pos_data_fields(self, config):
+        "Adds the custom field ak_customer_code to the pos data"""
         fields_list = super()._load_pos_data_fields(config)
         fields_list.append('ak_customer_code')
         return fields_list
